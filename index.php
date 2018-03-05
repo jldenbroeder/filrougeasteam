f5d05644ad30940330ca179bd3e44d09a8faf9ef
alixe82140 <alixe.boum@gmail.com>
2018-03-05 11:49:45 +0100

    lk

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>disquaire</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+</head>
+<body>
+	<div class="grid">
+		<div class="header">
+			<div class="nav">
+				<h2>navbar</h2>
+			</div>
+			<h1>header</h1>
+		</div>
+		<div class="presentation">
+			<h1>presentation</h1>
+		</div>
+		<div class="article">
+			<h1>articles</h1>
+		</div>
+		<div class="contact">
+			<h1>contact</h1>
+		</div>
+		<div class="footer">
+			<h1>footer</h1>
+		</div>
+	</div>
+</body>
+</html>
