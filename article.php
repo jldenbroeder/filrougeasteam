776949b16b6f82e59389791389e835422ba6af47
Domoso <hippolyte.malgaud@hotmail.fr>
2018-03-05 16:42:24 +0100

    Section articles presque terminée, il ne manque plus qu'à incurvé le text qui qui doit servir de lien

---
+++ article.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>disquaire</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="assets/css/article.css">
+	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
+</head>
+<body>
+	<section class="sectionArticle">
+		<h1>Les derniers articles du blog</h1>
+		<div class="gridArticle">
+			<div class="articles A">
+				<div class="boutonA">
+					<div class="textA">
+                    	<span class='w0'>R</span><span class='w1'>e</span><span class='w2'>a</span><span class='w3'>d</span><span class='w4'> </span><span class='w5'>M</span><span class='w6'>o</span><span class='w7'>r</span><span class='w8'>e</span>
+                    </div>
+				</div>
+			</div>
+			<div class="articles B">
+				<div class="boutonB">
+					
+				</div>
+			</div>
+			<div class="articles C">
+				<div class="boutonC">
+					
+				</div>
+			</div>
+			<div class="articles D">
+				<div class="boutonD">
+					
+				</div>
+			</div>
+			<div class="articles E">
+				<div class="boutonE">
+					
+				</div>
+			</div>
+			<div class="articles F">
+				<div class="boutonF">
+					
+				</div>
+			</div>
+			<div class="articles G">
+				<div class="boutonG">
+					
+				</div>
+			</div>
+			<div class="articles H">
+				<div class="boutonH">
+					
+				</div>
+			</div>
+		</div>
+		<h5>Venez voir notre blog</h5>
+	</section>
+</body>
+</html>
