6247c5d55a4294bcc234114dd5447b1c39c58c5c
Domoso <hippolyte.malgaud@hotmail.fr>
2018-03-06 12:10:04 +0100

    Sections article et footer terminées

--- article.php
+++ article.php
@@ -12,48 +12,46 @@
 		<div class="gridArticle">
 			<div class="articles A">
 				<div class="boutonA">
-					<div class="textA">
-                    	<span class='w0'>R</span><span class='w1'>e</span><span class='w2'>a</span><span class='w3'>d</span><span class='w4'> </span><span class='w5'>M</span><span class='w6'>o</span><span class='w7'>r</span><span class='w8'>e</span>
-                    </div>
+					<a href=""><p class="lienBlog">Read more</p></a>
 				</div>
 			</div>
 			<div class="articles B">
 				<div class="boutonB">
-					
+					<a href=""><p class="lienBlog">Read more</p></a>
 				</div>
 			</div>
 			<div class="articles C">
 				<div class="boutonC">
-					
+					<a href=""><p class="lienBlog">Read more</p></a>
 				</div>
 			</div>
 			<div class="articles D">
 				<div class="boutonD">
-					
+					<a href=""><p class="lienBlog">Read more</p></a>
 				</div>
 			</div>
 			<div class="articles E">
 				<div class="boutonE">
-					
+					<a href=""><p class="lienBlog">Read more</p></a>
 				</div>
 			</div>
 			<div class="articles F">
 				<div class="boutonF">
-					
+					<a href=""><p class="lienBlog">Read more</p></a>
 				</div>
 			</div>
 			<div class="articles G">
 				<div class="boutonG">
-					
+					<a href=""><p class="lienBlog">Read more</p></a>
 				</div>
 			</div>
 			<div class="articles H">
 				<div class="boutonH">
-					
+					<a href=""><p class="lienBlog">Read more</p></a>
 				</div>
 			</div>
 		</div>
-		<h5>Venez voir notre blog</h5>
+		<a href=""><h5>Venez voir notre blog</h5></a>
 	</section>
 </body>
 </html>
