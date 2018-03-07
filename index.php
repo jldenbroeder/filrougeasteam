43de9732c89a2eeff70cd38634ea445fbc8e17e6
Domoso <hippolyte.malgaud@hotmail.fr>
2018-03-07 14:42:27 +0100

    Correction des problèmes de la branch Hippolyte

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 	<title>disquaire</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/style-nav.css">
+  <link rel="stylesheet" type="text/css" href="assets/css/style-head.css">
 	<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">
 </head>
 <body>
